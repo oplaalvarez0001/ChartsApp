09b65dbc086d6e089ca2a0ed88e7f79ec8a01f33
Oscar Pla <oplaalvarez@microsoft.com>
2022-10-04 09:49:44 -0400

    End of section 3

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'Charts App';
+  users: any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+    getUsers(){
+      this.http.get("https://localhost:5001/api/users").subscribe({
+        next: response => this.users = response,
+        error: error => console.log(error)
+      })
+    }
+}
+
