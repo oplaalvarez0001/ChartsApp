08f8346940abd80fd68874b4270a456a6fd65e19
Oscar Pla <oplaalvarez@microsoft.com>
2022-11-08 15:35:15 -0500

    Added login backen functionality

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace API.Controllers
 {
